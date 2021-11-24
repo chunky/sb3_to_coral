2243e18464e2dbb2c9f38c70458bfe2b63368db2
Gary Briggs <gbriggs@rand.org>
2021-11-24 15:04:42 -0800

    Unifying command line args

--- run_sb3.py
+++ run_sb3.py
@@ -4,18 +4,17 @@ import gym
 from stable_baselines3 import SAC
 
 if __name__ == '__main__':
-    env_name = "MountainCarContinuous-v0"
-    model_save_file = "model.zip"
+    env_name = 'MountainCarContinuous-v0'
+    model_prefix = 'model'
     if len(sys.argv) < 3:
-        print("Usage: " + str(sys.argv[0]) + " <envname> <model_save_file>")
-        print(" Defaulting to env: " + env_name + ", savefile: " + model_save_file)
+        print("Usage: " + str(sys.argv[0]) + " <envname> <model_prefix>")
+        print(" Defaulting to env: " + env_name + ", model_prefix: " + model_prefix)
     else:
         env_name = sys.argv[1]
-        model_save_file = sys.argv[2]
+        model_prefix = sys.argv[2]
+    model_save_file = model_prefix + ".zip"
 
     env = gym.make(env_name)
-    env.reset()
-    env.render()
     obs = env.reset()
 
     model = SAC.load(model_save_file, env)
