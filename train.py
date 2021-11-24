f3cf39794367ff98610b6de5c95e3439bc83f67f
Gary Briggs <gbriggs@rand.org>
2021-11-24 14:59:10 -0800

    Bits and bobs

--- train.py
+++ train.py
@@ -1,31 +1,22 @@
-import os
 import sys
 import gym
-import numpy as np
 
 from stable_baselines3 import SAC
 
 if __name__ == '__main__':
-    env_name = "MountainCarContinuous-v0"
-    model_save_file = "model.zip"
+    env_name = 'MountainCarContinuous-v0'
+    model_prefix = 'model'
     if len(sys.argv) < 3:
-        print("Usage: " + str(sys.argv[0]) + " <envname> <model_save_file>")
-        print(" Defaulting to env: " + env_name + ", savefile: " + model_save_file)
+        print("Usage: " + str(sys.argv[0]) + " <envname> <model_prefix>")
+        print(" Defaulting to env: " + env_name + ", model_prefix: " + model_prefix)
     else:
         env_name = sys.argv[1]
-        model_save_file = sys.argv[2]
-
+        model_prefix = sys.argv[2]
+    model_save_file = model_prefix + ".zip"
     env = gym.make(env_name)
     env.reset()
     env.render()
 
     model = SAC('MlpPolicy', env, verbose=1)
-    model.learn(total_timesteps=25_000)
+    model.learn(total_timesteps=250_000)
     model.save(model_save_file)
-
-    obs = env.reset()
-    for _ in range(1000):
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        env.render()
-
