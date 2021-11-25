616dddb69b18d7950bc72ec5e2c2b6faca9e0d34
Gary Briggs <gbriggs@rand.org>
2021-11-24 21:03:00 -0800

    Comment on estimated usefulness of this training

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ import gym
 
 from stable_baselines3 import SAC
 
+# This is here so as to generate a model.zip file; I don't tune the parameters or even expect it to
+#  generate an agent that works usefully, I just want a complete-and-intact saved model.
+# At the very least, increase number of timesteps to model.learn if you need something useful
+
 if __name__ == '__main__':
     env_name = 'MountainCarContinuous-v0'
     model_prefix = 'model'
@@ -18,5 +22,6 @@ if __name__ == '__main__':
     env.render()
 
     model = SAC('MlpPolicy', env, verbose=1)
-    model.learn(total_timesteps=250_000)
+    model.learn(total_timesteps=25)
+    # model.learn(total_timesteps=250_000)
     model.save(model_save_file)
